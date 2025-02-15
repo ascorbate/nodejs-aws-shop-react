f59fa839c35ab81aac8488e7517e51db93b6bd36
ascorbate <hogdogcar@gmail.com>
2025-02-15 19:57:41 +0100

    feat:cdk code for deploy

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,57 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  
+    const BUCKET_NAME = 'course-rsschool-shop-website';
+    const DISTRIBUTION_COMMENT = 'CloudFront Distribution for Animal Shop';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: BUCKET_NAME,
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+      autoDeleteObjects: true, 
+    });
+
+  
+    const websiteOAI = new cloudfront.OriginAccessIdentity(this, 'WebsiteOAI', {
+      comment: DISTRIBUTION_COMMENT,
+    });
+
+ 
+    websiteBucket.grantRead(websiteOAI);
+
+
+    const websiteDistribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket, { originAccessIdentity: websiteOAI }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+      comment: DISTRIBUTION_COMMENT,
+    });
+
+ 
+    new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
+      destinationBucket: websiteBucket,
+      sources: [s3Deployment.Source.asset('../dist')],
+      distribution: websiteDistribution,
+      distributionPaths: ['/*'],
+    });
   }
 }
